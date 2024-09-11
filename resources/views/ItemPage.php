07ef2f4ce78e83f7c6293e08f55e30732b78799e
JohnJomarr <johnjomarr16@gmail.com>
2024-09-11 17:48:09 +0800

    First commit

---
+++ resources/views/ItemPage.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Item Page</title>
+</head>
+<style>
+        .container {
+            max-width: 1200px;
+            margin: 0 auto;
+            padding: 20px;
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+            gap: 20px;
+        }
+
+        .box {
+            background-color: #f4f4f4;
+            border: 1px solid #ccc;
+            padding: 20px;
+            width: 200px;
+            text-align: center;
+            box-shadow: 2px 2px 12px rgba(0, 0, 0, 0.1);
+            transition: transform 0.3s;
+        }
+
+        .box:hover {
+            transform: scale(1.05);
+        }
+
+        .box a {
+            text-decoration: none;
+            color: #333;
+            font-weight: bold;
+        }
+    </style>
+<body>
+    <div class="container">
+        <div class="box">
+            <a href="https://www.google.com/search?client=ubuntu-sn&hs=xBO&sca_esv=9783522cabc36d5f&sca_upv=1&channel=fs&q=ninja+turtle&udm=2&fbs=AEQNm0B8dVdIWR07uWWlg1TdKnNtA1cwMugrQsIKmAo5AEZHWRFlUeGLxYlhagMfUatSvHu3MSamP9Qd2SfjyZyVIdPFrZFmdorP0BQX-5QUvERZ7CgntLysKxPYR85LNkkQ-ODVQlzCBgHDwYGwBEtb1wyzIiqYOAGOFOhRLG73H-MUdJY1ZFjTgiSsk2gQgTHDHU_Mnn5ewYy4nGfZAENFgsXyYdMtYQ&sa=X&ved=2ahUKEwivqKG2ybqIAxUIxTgGHYSULNIQtKgLegQIDRAB&biw=1296&bih=656&dpr=1#vhid=cAYBPOlQSUnQIM&vssid=mosaic">Link 1</a>
+        </div>
+        <div class="box">
+            <a href="https://www.google.com/search?client=ubuntu-sn&hs=xBO&sca_esv=9783522cabc36d5f&sca_upv=1&channel=fs&q=ninja+turtle&udm=2&fbs=AEQNm0B8dVdIWR07uWWlg1TdKnNtA1cwMugrQsIKmAo5AEZHWRFlUeGLxYlhagMfUatSvHu3MSamP9Qd2SfjyZyVIdPFrZFmdorP0BQX-5QUvERZ7CgntLysKxPYR85LNkkQ-ODVQlzCBgHDwYGwBEtb1wyzIiqYOAGOFOhRLG73H-MUdJY1ZFjTgiSsk2gQgTHDHU_Mnn5ewYy4nGfZAENFgsXyYdMtYQ&sa=X&ved=2ahUKEwivqKG2ybqIAxUIxTgGHYSULNIQtKgLegQIDRAB&biw=1296&bih=656&dpr=1#vhid=YjbbWgmhPtqPFM&vssid=mosaic">Link 2</a>
+        </div>
+        <div class="box">
+            <a href="https://www.google.com/search?client=ubuntu-sn&hs=xBO&sca_esv=9783522cabc36d5f&sca_upv=1&channel=fs&q=ninja+turtle&udm=2&fbs=AEQNm0B8dVdIWR07uWWlg1TdKnNtA1cwMugrQsIKmAo5AEZHWRFlUeGLxYlhagMfUatSvHu3MSamP9Qd2SfjyZyVIdPFrZFmdorP0BQX-5QUvERZ7CgntLysKxPYR85LNkkQ-ODVQlzCBgHDwYGwBEtb1wyzIiqYOAGOFOhRLG73H-MUdJY1ZFjTgiSsk2gQgTHDHU_Mnn5ewYy4nGfZAENFgsXyYdMtYQ&sa=X&ved=2ahUKEwivqKG2ybqIAxUIxTgGHYSULNIQtKgLegQIDRAB&biw=1296&bih=656&dpr=1#vhid=vPzUwIYA5QUKOM&vssid=mosaic">Link 3</a>
+        </div>
+    </div>
+</body>
+</html>
